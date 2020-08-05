28261ea38b69b18d009a1db261af3f216156cb3b
Ben Senescu <senescu.b@husky.neu.edu>
2020-08-04 20:09:38 -0400

    seperated into functions and added error checking

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
+import os
 import sys
 import time
-import csv 
+import csv
 from datetime import datetime
 
 from selenium import webdriver
@@ -8,113 +9,208 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import SessionNotCreatedException
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import StaleElementReferenceException
 
-if len(sys.argv) != 4 or sys.argv[1] == "--help":
-  print("-----------------Manual-----------------\n"
-    + "First Argument: Valid Soundcloud Playlist\n"
-    + "Second Argument: Csv to Edit: ex. artist.csv\n"
-    + "Third Argument: Is this a new File? Write w if so, a to append.\n")
-  sys.exit()
 
-def formatSoundcloudListens(str):
-  if "K" not in str and "M" not in str:
-    return str
-  if "K" in str:
-    return float(str[:-1]) * 1000
-  elif "M" in str:
-    return float(str[:-1]) * 1000 * 1000
-  
-playlistUrl = sys.argv[1]
+def check_args():
+    if len(sys.argv) != 4 or sys.argv[1] == "--help":
+        print("-----------------Manual-----------------\n"
+              + "First Argument: Valid Soundcloud Playlist\n"
+              + "Second Argument: Csv to Edit: ex. artist.csv\n"
+              + "Third Argument: Is this a new File? Write w if so, a to append.\n")
+        sys.exit()
 
-browser = webdriver.Safari()
-browser.maximize_window()
+def format_soundcloud_listens(str):
+    if "K" not in str and "M" not in str:
+        return str
+    if "K" in str:
+        return float(str[:-1]) * 1000
+    elif "M" in str:
+        return float(str[:-1]) * 1000 * 1000
 
-browser.get(playlistUrl)
-time.sleep(1)
+def get_playlist_songs(browser, playlistUrl, page_downs):
+    browser.get(playlistUrl)
+    time.sleep(1)
 
-elem = browser.find_element_by_tag_name("body")
-
-no_of_pagedowns = 10
-
-while no_of_pagedowns:
-    elem.send_keys(Keys.PAGE_DOWN)
-    time.sleep(0.2)
-    no_of_pagedowns-=1
-
-elems = browser.find_elements_by_css_selector('.sc-ministats-plays , .sc-link-dark.sc-font-light , .trackItem__username')
-
-playlistDataFormatted = []
-
-i = 0
-while i < len(elems) - 2:
-  playlistDataFormatted.append({
-    'artist': elems[i].text.strip(), 
-    'song': elems[i+1].text.strip(), 
-    'numberOfListens': formatSoundcloudListens(elems[i+2].text.strip()), 
-    'date': datetime.today().strftime('%Y-%m-%d'), 
-    'igHandle': 'n/a'})
-  i+=3
-
-for i in range(len(playlistDataFormatted)):
-  try:
     elem = browser.find_element_by_tag_name("body")
-    no_of_pagedowns = 10
+
+    no_of_pagedowns = page_downs
 
     while no_of_pagedowns:
-      elem.send_keys(Keys.PAGE_DOWN)
-      time.sleep(0.2)
-      no_of_pagedowns-=1  
-  
-    selector = ".sc-border-light-bottom:nth-child(%s) .sc-link-light" % str(i + 1)
-    browser.find_element_by_css_selector(selector).send_keys(Keys.RETURN)
-    time.sleep(1)
+        elem.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.2)
+        no_of_pagedowns -= 1
 
-    if len(browser.find_elements_by_css_selector(".sc-social-logo-instagram")) > 0:
-      browser.find_element_by_css_selector(".sc-social-logo-instagram").click()
-      time.sleep(1)
-      window_original = browser.window_handles[0]
-      window_after = browser.window_handles[1]
-      browser.switch_to.window(window_after)
-      time.sleep(1)
-      playlistDataFormatted[i]["igHandle"] = WebDriverWait(browser, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".fDxYl"))).text.strip()
-      # playlistDataFormatted[i]["igHandle"] = browser.find_element_by_css_selector(".fDxYl").text.strip()
-      #Close the tab or window
-      browser.close()
-      #Switch back to the old tab or window
-      browser.switch_to.window(window_original)
+    elems = browser.find_elements_by_css_selector(
+        '.m-disabled .trackItem__additional , .trackItem__blockMsg , .sc-ministats-plays , .sc-font-light , .trackItem__username,.sc - ministats - plays,.sc - link - dark.sc - font - light')
+    ''
+    playlistDataFormatted = []
 
-    browser.get(playlistUrl)
-    time.sleep(1)
-  except: 
+    i = 0
+    while i < len(elems) - 2:
+        playlistDataFormatted.append({
+            'artist': elems[i].text.strip(),
+            'song': elems[i + 1].text.strip(),
+            'numberOfListens': format_soundcloud_listens(elems[i + 2].text.strip()),
+            'date': datetime.today().strftime('%Y-%m-%d'),
+            'igHandle': 'n/a'})
+
+        if elems[i + 2].text.strip() == "Not available in United States":
+            i += 1
+
+        i += 3
+
+    return playlistDataFormatted
+
+def write_dict_csv(fields, dict):
+    with open(sys.argv[2], 'w') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
+        # writing the fields
+        csvwriter.writeheader()
+        # writing the data rows
+        for row in dict:
+            try:
+                csvwriter.writerow(row)
+            except UnicodeEncodeError:
+                print("ERROR: Could not write artist due to emoji", row.get("artist"))
+
+def append_dict_csv(fields, dict):
+    with open(sys.argv[2], 'a') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.DictWriter(csvfile, fieldnames=fields)
+        # writing the data rows
+        for row in dict:
+            try:
+                csvwriter.writerow(row)
+            except UnicodeEncodeError:
+                print("ERROR: Could not write artist due to emoji", row.get("artist"))
+
+def reset_browser(browser, playlistUrl):
+    print(browser.window_handles, browser.current_url)
     if len(browser.window_handles) > 1:
-      browser.close() 
-      browser.switch_to.window(browser.window_handles[0])
-    print("Error: Instagram data retrieval failed for %s." % playlistDataFormatted[i].get("artist"))
+        browser.close()
+        browser.switch_to.window(browser.window_handles[0])
+
     browser.get(playlistUrl)
 
-fields = ['artist', 'song', 'numberOfListens', 'date', 'igHandle'] 
+def switch_window_add_handle(browser, playlistDataDict, i, playlistUrl):
+    try:
+        window_original = browser.window_handles[0]
+        window_after = browser.window_handles[1]
+        browser.switch_to.window(window_after)
+        try:
+            playlistDataDict[i]["igHandle"] = WebDriverWait(browser, 3).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, ".fDxYl"))).text.strip()
 
-if (sys.argv[3] == "w"):
-  with open(sys.argv[2], 'w') as csvfile:
-    # creating a csv writer object  
-    csvwriter = csv.DictWriter(csvfile, fieldnames = fields )
-    # writing the fields  
-    csvwriter.writeheader()
-    # writing the data rows  
-    for row in playlistDataFormatted:
-      try:
-        csvwriter.writerow(row) 
-      except: 
-        print("ERROR: Could not write row.", row)
-elif (sys.argv[3] == "a"):
-  with open(sys.argv[2], 'a') as csvfile:
-    # creating a csv writer object  
-    csvwriter = csv.DictWriter(csvfile, fieldnames = fields )
-    # writing the data rows  
-    for row in playlistDataFormatted:
-      try:
-        csvwriter.writerow(row) 
-      except: 
-        print("ERROR: Could not write row.", row)
+        except NoSuchElementException:
+            print("Error: Instagram data retrieval failed for %s." % playlistDataDict[i].get("artist"))
+            reset_browser(browser, playlistUrl)
 
-browser.quit()
+        except TimeoutException:
+            try:
+                switch_window_add_handle(browser, playlistDataDict, i, playlistUrl)
+            except TimeoutException:
+                print("Error: Took too long to get IG for %s." % playlistDataDict[i].get("artist"))
+                reset_browser(browser, playlistUrl)
+
+
+        # playlistDataFormatted[i]["igHandle"] = browser.find_element_by_css_selector(".fDxYl").text.strip()
+        # Close the tab or window
+        browser.close()
+        # Switch back to the old tab or window
+        browser.switch_to.window(window_original)
+
+    except IndexError:
+        print("ERROR: No IG Window Opened for %s" % playlistDataDict[i].get("artist"))
+        reset_browser(browser, playlistUrl)
+
+
+# def manage_progress(progress_breaks, i):
+#     for bp in reversed(progress_breaks):
+#         if i == bp:
+#             try:
+#                 print("{0}% complete scraping your playlist!".format(round(bp / progress_breaks[len(progress_breaks) - 1])))
+#                 break
+#             except ZeroDivisionError:
+#                 break
+
+def scroll_body(browser, artistName, i, playlistUrl):
+    try:
+        print('test', artistName, browser.current_url)
+        elem = WebDriverWait(browser, 2).until(
+            EC.element_to_be_clickable((By.TAG_NAME, "body")))
+        no_of_pagedowns = i % 10
+
+        while no_of_pagedowns:
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(0.2)
+            no_of_pagedowns -= 1
+
+    except StaleElementReferenceException:
+        print("Error: Stale Element Exception for %s at url %s." % (artistName, browser.current_url))
+        reset_browser(browser, playlistUrl)
+
+def get_ig_handles(browser, playlistDataDict, playlistUrl):
+    for i in range(len(playlistDataDict)):
+        # manage_progress(progress_breaks, i)
+
+        scroll_body(browser, playlistDataDict[i].get("artist"), i, playlistUrl)
+
+        try:
+            selector = ".sc-border-light-bottom:nth-child(%s) .sc-link-light" % str(i + 1)
+            WebDriverWait(browser, 2).until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, selector))).send_keys(Keys.RETURN)
+
+            try:
+                WebDriverWait(browser, 1).until(
+                    EC.presence_of_element_located((By.CSS_SELECTOR, ".sc-social-logo-instagram"))).click()
+                switch_window_add_handle(browser, playlistDataDict, i, playlistUrl)
+            except TimeoutException:
+                print('No Instagram')
+                reset_browser(browser, playlistUrl)
+
+        except TimeoutException:
+            print("list not loaded")
+            reset_browser(browser, playlistUrl)
+
+        reset_browser(browser, playlistUrl)
+
+    return playlistDataDict
+
+def main():
+    check_args()
+    playlistUrl = sys.argv[1]
+
+    browser = webdriver.Safari()
+    print('Successfully started scraping your playlist!!')
+    browser.maximize_window()
+
+    playlistDataDict = get_playlist_songs(browser, playlistUrl, page_downs=10)
+    print(playlistDataDict)
+
+    # progress_breaks = [x * round((float(len(playlistDataDict)) / 10) * 100) for x in range(10)]
+    # # if len(playlistDataDict) < 10:
+    # #     progress_breaks = []
+
+    print(len(playlistDataDict))
+    playlistDataWithIG = get_ig_handles(browser, playlistDataDict, playlistUrl)
+
+    fields = ['artist', 'song', 'numberOfListens', 'date', 'igHandle']
+    if (sys.argv[3] == "w"):
+        write_dict_csv(fields, playlistDataWithIG)
+
+    elif (sys.argv[3] == "a"):
+        append_dict_csv(fields, playlistDataWithIG)
+
+    browser.quit()
+
+try:
+    main()
+except SessionNotCreatedException:
+    print('ERROR: Session Not Created Exception, The Safari instance is already paired with another WebDriver session.')
+    os.system('pkill safaridriver')
+    print('Please make sure to close any windows with safari open!')
